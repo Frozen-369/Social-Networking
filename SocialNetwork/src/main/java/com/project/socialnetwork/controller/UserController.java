40a20237a7dd94a00097aa426179f8b61fcbe49d
Frozen-369 <frozenboyarp@gmail.com>
2024-03-31 16:44:58 +0545

    Made Endpoint for updating userProfile

--- SocialNetwork/src/main/java/com/project/socialnetwork/controller/UserController.java
+++ SocialNetwork/src/main/java/com/project/socialnetwork/controller/UserController.java
@@ -3,6 +3,7 @@ package com.project.socialnetwork.controller;
 import com.project.socialnetwork.entity.User;
 import com.project.socialnetwork.entity.UserProfile;
 import com.project.socialnetwork.service.UserService;
+import com.project.socialnetwork.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +16,64 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/registerUser")
-    public ResponseEntity<User> registerUser(@RequestBody User user) {
-        User savedUser = userService.registerUser(user);
-        return ResponseEntity.ok(savedUser);
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        userService.registerUser(user);
+        return ResponseEntity.ok().body(ResponseUtils.USER_SUCCESS);
     }
 
+<<<<<<< Updated upstream
     @PostMapping("/updateProfile/{user_id}")
     public ResponseEntity<UserProfile> updateProfile(@PathVariable("user_id") Long user_id, @RequestBody UserProfile userProfile){
         UserProfile updatedProfile = userService.updateProfile(userProfile, user_id);
         return ResponseEntity.ok(updatedProfile);
     }
 
+=======
+    @GetMapping("/getUser/{user_id}")
+    public ResponseEntity<String> getUserById(@PathVariable Long user_id){
+        User user = userService.getUserById(user_id);
+        return ResponseEntity.ok().body(user.toString());
+
+    }
+
+    @PostMapping("/{user_id}/createProfile")
+    public ResponseEntity<String> createProfile(@RequestBody UserProfile userProfile, @PathVariable Long user_id){
+        userService.createProfile(userProfile,user_id);
+        return ResponseEntity.ok().body(ResponseUtils.USER_PROFILE_CREATE);
+    }
+
+    @GetMapping("/getUserProfile/{userProfile_id}")
+    public ResponseEntity<String> getUserProfileById(@PathVariable Long userProfile_id){
+        User user = userService.getUserById(userProfile_id);
+        return ResponseEntity.ok().body(user.toString());
+
+    }
+
+    @PutMapping("/updateProfile")
+    public ResponseEntity<String> updateProfile(@RequestBody UserProfile userProfile) throws Exception {
+
+        userService.updateProfile(userProfile);
+        return ResponseEntity.ok().body(ResponseUtils.USER_PROFILE_UPDATE);
+    }
+
+    @PostMapping("/post/{user_id}")
+    public ResponseEntity<String> post(@RequestBody Post postDetails, @PathVariable Long user_id){
+        Post post = userService.postDetails(postDetails, user_id);
+        return ResponseEntity.ok().body(ResponseUtils.POST_CREATE);
+    }
+
+    @GetMapping("/getPost/{post_id}")
+    public ResponseEntity<String> getPostById(@PathVariable Long post_id){
+        Post post = userService.getPostById(post_id);
+        return ResponseEntity.ok().body(post.toString());
+
+    }
+
+    @DeleteMapping("/post/{post_id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long post_id){
+        userService.deletePost(post_id);
+        return ResponseEntity.ok().body(ResponseUtils.DELETE);
+    }
+
+>>>>>>> Stashed changes
 }
--- SocialNetwork/src/main/java/com/project/socialnetwork/service/UserService.java
+++ SocialNetwork/src/main/java/com/project/socialnetwork/service/UserService.java
@@ -20,6 +20,7 @@ public class UserService {
         return userDao.save(user);
     }
 
+<<<<<<< Updated upstream
     public UserProfile updateProfile(UserProfile userProfile, Long user_id){
         User user = userDao.findById(user_id).get();
         userProfile.setUser(user);
@@ -27,4 +28,79 @@ public class UserService {
     }
 
 
+=======
+
+    public User getUserById(Long id){
+        User user = userDao.findById(id).orElse(null);
+        if(user == null) {
+            throw new NullPointerException("User is empty");
+        }
+        return user;
+    }
+    public UserProfile createProfile(UserProfile userProfile, Long userId){
+        Optional<User> userOptional = Optional.ofNullable(getUserById(userId));
+        if(userOptional.isPresent()){
+            User user = userOptional.get();
+            userProfile.setUser(user);
+            return userProfileDao.save(userProfile);
+        }
+        else{
+            throw new NotFoundException("User not found");
+        }
+
+    }
+
+    public UserProfile getUserProfileById(Long id){
+        UserProfile userProfile = userProfileDao.findById(id).orElse(null);
+        if(userProfile == null) {
+            throw new NullPointerException("User is empty");
+        }
+        return userProfile;
+    }
+
+    public void updateProfile(UserProfile userProfile) throws Exception {
+        Optional<UserProfile> userProfileOptional = Optional.ofNullable(getUserProfileById(userProfile.getProfile_id()));
+        if(userProfileOptional.isPresent()){
+
+            UserProfile userProfile1 = userProfileOptional.get();
+
+            userProfile1.setFirstname(userProfile.getFirstname() != null ? userProfile.getFirstname(): userProfile1.getFirstname());
+            userProfile1.setLastname(userProfile.getLastname() != null ? userProfile.getLastname(): userProfile1.getLastname());
+            userProfile1.setBio(userProfile.getAddress() != null ? userProfile.getAddress(): userProfile1.getAddress());
+            userProfile1.setProfile_pic_url(userProfile.getProfile_pic_url() != null ? userProfile.getProfile_pic_url(): userProfile1.getProfile_pic_url());
+            userProfile1.setBirthday(userProfile.getBirthday() != null ? userProfile.getBirthday(): userProfile1.getBirthday());
+
+            userProfileDao.save(userProfile1);
+
+        }
+        else {
+            throw new NotFoundException("User not found.");
+        }
+    }
+
+
+    public Post postDetails(Post postDetails, Long user_id) {
+        User user = userDao.findById(user_id).get();
+        postDetails.setUser(user);
+        return postDao.save(postDetails);
+    }
+
+    public Post getPostById(Long postId) {
+        Post post = postDao.findById(postId).orElse(null);
+        if(post == null) {
+            throw new NullPointerException("User is empty");
+        }
+        return post;
+    }
+
+    public void deletePost(Long postId) {
+        Post post = getPostById(postId);
+        if(post == null){
+            throw new NullPointerException("Id cannot be empty");
+        }
+        postDao.delete(post);
+    }
+
+
+>>>>>>> Stashed changes
 }
